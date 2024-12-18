5fe219608469c9a25140b55468e2e864565288de
Pavel Vnuchkov <pavelvnuckov.com>
2024-12-18 18:01:42 +0300

    Initial commit

---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,60 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+  private PostController controller;
+
+  @Override
+  public void init() {
+    final var repository = new PostRepository();
+    final var service = new PostService(repository);
+    controller = new PostController(service);
+    System.out.println("инициализировано");
+  }
+
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) {
+    // если деплоились в root context, то достаточно этого
+    try {
+      final var path = req.getRequestURI();
+      final var method = req.getMethod();
+      System.out.println(path);
+      System.out.println(method);
+      // primitive routing
+      if (method.equals("GET") && path.equals("/api/posts")) {
+        controller.all(resp);
+        return;
+      }
+      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+        // easy way
+        String longId = path.substring(path.lastIndexOf("/") + 1);
+        System.out.println(longId);
+        final var id = Long.parseLong(longId);
+        controller.getById(id, resp);
+        return;
+      }
+      if (method.equals("POST") && path.equals("/api/posts")) {
+        controller.save(req.getReader(), resp);
+        return;
+      }
+      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+        // easy way
+        final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+        controller.removeById(id, resp);
+        return;
+      }
+      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    } catch (Exception e) {
+      e.printStackTrace();
+      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+}
+
